0932a230bc88fe2b1ca39d49039ccd324f9048cf
Діма Калинюк <dima.kalinyuk.2019@ukr.net>
2020-06-06 20:12:47 +0300

    Laba2Task1

---
+++ Laba2Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Laba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char day = (char)Console.Read();
+            Console.ReadLine();
+            switch (day)
+            {
+                case '1': Console.WriteLine("Понедiлок"); break;
+                case '2': Console.WriteLine("Вiвторок"); break;
+                case '3': Console.WriteLine("Середа"); break;
+                case '4': Console.WriteLine("Четвер"); break;
+                case '5': Console.WriteLine("П'ятниця"); break;
+                case '6': Console.WriteLine("Субота"); break;
+                case '7': Console.WriteLine("Недiля"); break;
+            }
+            Console.Read();
+        }
+    }
+}
